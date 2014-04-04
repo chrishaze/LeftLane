0867976aea255cbe4806d16ecee7616a804f8334
chrishaze <chrishaze88@gmail.com>
2014-04-04 04:27:01 -0400

    Initial Commit
    Blank single view application

---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LeftLane
+//
+//  Created by Chris Hayes on 4/4/14.
+//  Copyright (c) 2014 CAV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LeftLane
+//
+//  Created by Chris Hayes on 4/4/14.
+//  Copyright (c) 2014 CAV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
